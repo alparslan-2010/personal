776c218050a6ca40b2d8185f96939228ad9df07e
alparslan-2010 <alparslannihatt@gmail.com>
2023-04-27 22:50:20 +0200

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Footer from './layout/footer';
+import Header from './layout/header';
+import Contact from './pages/contact';
+import Home from './pages/home';
+import Project from './pages/project';
+import Resume from './pages/resume';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+    
+    
+    <>
+
+
+     <Header/>
+    
+        <Router>
+
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/resume' element={<Resume />} />
+        <Route path='/projects' element={<Project/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+
+      </Routes>
+    </Router>
+
+    <Footer/>
+    </>
+    )
 }
 
 export default App;
